1923479fde44d88fbb20487f04d089d8801f8512
ernestocr <ernesto_camacho@ymail.com>
2023-05-13 18:42:00 -0600

    mubs are created

---
+++ prog/even_mubs.py
@@ -0,0 +1,40 @@
+from sage.all import *
+import numpy as np
+
+p = 2
+n = 2
+q = p**n
+
+# GR(4,2)
+R  = PolynomialRing(Integers(4), 't')
+t  = R.gen()
+GR = R.quotient(t**2+t+1, 'w')
+w  = GR.gen()
+
+def ringTrace(a, b):
+    s = GR(0)
+    for k in range(n):
+        s += a**(2**k) + GR(2)*b**(2**k)
+    return s 
+
+# Teichmüller
+T = [0] + [w**k for k in range((2**n - 2) + 1)]
+
+def brute2adic(x):
+    for a in T:
+        for b in T:
+            if x == a + GR(2)*b:
+                return (a,b)
+
+def createMubs(q):
+    mubs = [identity_matrix(q)]
+    for m in T:
+        Bm = zero_matrix(SR, q, q)
+        for j, v in enumerate(T):
+            for i, w in enumerate(T):
+                a, b = brute2adic(w**2 * m + 2 * w * v)
+                pwr  = Integer(lift(ringTrace(a, b)))
+                Bm[i,j] = 1/sqrt(q) * I**pwr
+        mubs.append(Bm)
+    return mubs
+
---
+++ prog/mubs_utils.py
@@ -0,0 +1,51 @@
+from sage.all import *
+import numpy as np
+
+def testMubs(mubs):
+    d = mubs[0].nrows()
+    for b, B in enumerate(mubs):
+        print('Testing set', b)
+        for v, V in enumerate(mubs):
+            if b != v:
+                for j in range(d):
+                    for k in range(d):
+                        r = (transpose(B[:,j].conjugate()) * V[:,k])[0,0]
+                        r = (r.abs()**2).simplify()
+                        if r != 1/d:
+                            print(r, b, v, j, k)
+                            raise Exception('Not MUBs!')
+    return True
+
+def testMubs2(mubs):
+    d = mubs[0].nrows()
+    y = (1/d) * ones_matrix(SR, d)
+    for b, B in enumerate(mubs):
+        print('Testing set', b)
+        for v, V in enumerate(mubs):
+            if b != v:
+                x = B.conjugate_transpose() * V
+                x = x.apply_map(lambda z: (z.abs()**2).simplify())
+                if x != y:
+                    print(b, v)
+                    raise Exception('Not MUBs!')
+    return True
+
+# def testMubsNp(mubs):
+#     d = mubs[0].nrows()
+#     mubs = [m.numpy(dtype='complex64) for m in mubs]
+#     cmp  = np.ones((d,d)) / d
+#     for b, B in enumerate(mubs):
+#         for v, V in enumerate(mubs):
+#             if b != v:
+#                 X = B.conj().T @ V
+#                 X = np.abs(X)**2
+#                 if not np.allclose(X, cmp):
+#                     print(b, v)
+#                     raise Exception('Not MUBs!')
+#     return True
+
+def saveMubs(mubs, path):
+    nmubs = [m.numpy(dtype='complex64') for m in mubs]
+    np.save(path, np.concatenate(nmubs))
+    print('Saved to', path)
+    pass
